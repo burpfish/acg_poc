deeb05209c26586ffcb8f2731ead3425c30e929e
burpfish <burforddave@yahoo.com>
2022-10-21 12:38:28 +0000

    minor updates

--- iac/Lambda Proxy/0_clean_up.sh
+++ iac/Lambda Proxy/0_clean_up.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env sh
 
+read -p 'Enter burfordfc private key: ' KEY
+echo "$KEY" > ./main_deployment/lb/burfordfc.com_private_key.key
+
 # Clean up (assumes the workspace has expired - i.e. we don't call tf destroy)
 rm terraform.tfstate -f
 aws configure
